b56a5d2f76408091b9d6b43f92290c4931c6ea7f
Maksym <makximchagin@gmail.com>
2026-04-16 16:25:20 +0300

    register ImageManager binding

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void
     {
-        //
+        $this->app->bind(ImageManager::class, fn() => ImageManager::usingDriver(Driver::class));
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+
     }
 }
