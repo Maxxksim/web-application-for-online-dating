8a135164d29f5074d9f6e3af6cbbe485f4430cc2
Maksym <makximchagin@gmail.com>
2026-04-19 00:50:00 +0300

    use Illuminate\Http\Request instead of facade

--- api/app/Http/Controllers/ProfileController.php
+++ api/app/Http/Controllers/ProfileController.php
@@ -2,18 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfilePhotoRequest;
 use App\Http\Requests\UpdateProfileRequest;
 use App\Http\Resources\ProfileResource;
-use App\Jobs\ProcessValidatePhoto;
 use App\Models\Profile;
-use App\Models\ProfilePhoto;
-use App\Services\PhotoService;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Storage;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
 
 
 class ProfileController extends Controller
