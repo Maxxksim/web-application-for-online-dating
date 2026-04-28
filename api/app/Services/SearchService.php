338c5961042ed640b4d130f83c7d0b8f3a4b5a65
Maksym <makximchagin@gmail.com>
2026-04-28 16:30:45 +0300

    fix search

--- api/app/Services/SearchService.php
+++ api/app/Services/SearchService.php
@@ -17,11 +17,11 @@ class SearchService
     {
 
         $searchFilters = $profile->searchFilters;
-        $currentGeoPoint = $profile->geolocation->geo_point;
 
         return Profile::join('geolocations', 'profiles.id', '=', 'geolocations.profile_id')
-            ->whereRaw("ST_DWithin(geolocations.geo_point::geography, {$currentGeoPoint}::geography, ?)", [
-                $searchFilters['distance'] * 1000
+            ->whereRaw("ST_DWithin(geolocations.geo_point::geography, (SELECT geo_point FROM geolocations WHERE profile_id = ?)::geography, ?)", [
+                $profile->id,
+                $searchFilters->distance * 1000
             ])
             ->where('profiles.id', '!=', $profile->id)
             ->whereRaw("DATE_PART('year', AGE(profiles.date_of_birth)) BETWEEN ? AND ?", [
@@ -30,7 +30,9 @@ class SearchService
             ])
             ->where('profiles.gender', $searchFilters['gender'])
             ->select('profiles.*')
-            ->selectRaw("ST_Distance(geolocations.geo_point::geography, {$currentGeoPoint}::geography) / 1000 as distance")
+            ->selectRaw("ST_Distance(geolocations.geo_point::geography, (SELECT geo_point FROM geolocations WHERE profile_id = ?)::geography) / 1000 as distance", [
+                $profile->id
+            ])
             ->orderBy('distance')
             ->paginate(20);
     }
