51e8ffff0c88fd4e91bb55a94e8ba77a5156d0fb
Maksym <makximchagin@gmail.com>
2026-05-30 18:49:36 +0300

    fix rollback swipe

--- api/app/Models/Profile.php
+++ api/app/Models/Profile.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
-#[Fillable(['user_id', 'name', 'date_of_birth', 'gender', 'country', 'city', 'description', 'relevance_score', 'completion_percentage', 'is_enabled', 'relevance_score_updated_on', 'dating_purpose'])]
+#[Fillable(['user_id', 'name', 'date_of_birth', 'gender', 'country', 'city', 'description', 'relevance_score', 'completion_percentage', 'is_enabled', 'relevance_score_updated_on', 'dating_purpose', 'height', 'weight', 'body_type', 'eye_color', 'hair_color', 'smoking', 'drinking', 'children', 'zodiac_sign', 'exercise'])]
 class Profile extends Model
 {
     public function user(): BelongsTo
