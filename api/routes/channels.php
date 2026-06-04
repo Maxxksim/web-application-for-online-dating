3e57fade352ddaea9c87e1740df0ff2a9cc99296
Maksym <makximchagin@gmail.com>
2026-06-04 20:24:49 +0300

    update requests add nullable rule

--- api/routes/channels.php
+++ api/routes/channels.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Broadcast;
 
 
 Broadcast::channel('notifications.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $userId;
+    return (int)$user->id === (int)$userId;
 });
 
 Broadcast::channel('likesRetracted.{userId}', function ($user, $userId) {
@@ -13,6 +13,10 @@ Broadcast::channel('likesRetracted.{userId}', function ($user, $userId) {
 
 Broadcast::channel('chats.{userId}', function ($user, $userId) {
     return (int)$user->id === (int)$userId;
+});
+
+Broadcast::channel('profiles.{profileId}', function ($user, $profileId) {
+    return (int)$user->profile->id === (int)$profileId;
 });
 
 
